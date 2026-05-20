d9e572de34e2819e73db0f1f66b74c95577245f9
Maxym Fediw <362706764@GoTVDSB.ca>
2026-05-20 13:23:10 -0400

    Checkpoint #4

--- Game1.cs
+++ Game1.cs
@@ -4,6 +4,16 @@ using Microsoft.Xna.Framework.Input;
 
 namespace Delivery_Dog_Fight
 {
+
+    enum Screen 
+    {
+        LevelSelect, 
+        TipScreen,
+        Level1,
+        Level2,
+        Level3,
+        Done
+    }
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
@@ -14,11 +24,16 @@ namespace Delivery_Dog_Fight
 
         MouseState mouseState, prevMouseState;
 
-        Texture2D introJetTexture, cockpitTexture, upsJetTexture, fedExJetTexture, explosionTexture;
+        Texture2D introJetTexture, cockpitTexture, upsJetTextureBig, upsJetTextureSmall, fedExJetTextureBig, fedExJetTextureSmall, explosionTexture;
 
-        Rectangle upsJetBigRect, upsJetSmallRect, fedExBigRect, fedExSmallRect, introJetRect, window;
+        Rectangle upsJetBigRect, upsJetSmallRect, fedExBigRect, fedExSmallRect, introJetRect, window, cockpitRect;
 
         SpriteFont textFont;
+
+        bool level1 = false, level2 = false, level3 = false;
+
+        
+
 
         public Game1()
         {
@@ -34,21 +49,23 @@ namespace Delivery_Dog_Fight
 
 
          //                       X, Y, Width, Height
-            window = new Rectangle(0, 0, 500, 355);
+            window = new Rectangle(0, 0, 1000, 710);    //500, 355
 
             _graphics.PreferredBackBufferHeight = window.Height;
             _graphics.PreferredBackBufferWidth = window.Width;
             _graphics.ApplyChanges();
 
-            upsJetBigRect = new Rectangle(20, 20, 150, 49);
+            upsJetBigRect = new Rectangle(20, 20, 300, 98);
 
-            fedExBigRect = new Rectangle(20, 40, 150, 49);
+            fedExBigRect = new Rectangle(20, 40, 300, 98);  //   Was:  150, 49 --- Now: 300, 98
 
-            upsJetSmallRect = new Rectangle (20, 60, 100, 32);
+            upsJetSmallRect = new Rectangle (40, 160, 100, 32);
 
             fedExSmallRect = new Rectangle(20, 80, 100, 32);
 
             introJetRect = new Rectangle(200, 100, 400, 136);
+
+            cockpitRect = new Rectangle(0, 10, 1000, 710);  // Smaller: 0, 20, 500, 355   Bigger: 0, 10, 1000, 710
 
 
             base.Initialize();
@@ -58,6 +75,20 @@ namespace Delivery_Dog_Fight
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
+            introJetTexture = Content.Load<Texture2D>("IntroJet");
+
+            cockpitTexture = Content.Load<Texture2D>("CockpitTransparentDone");
+
+            upsJetTextureBig = Content.Load<Texture2D>("UPSJetBig");
+
+            upsJetTextureSmall = Content.Load<Texture2D>("UPSJetSmall");
+
+            fedExJetTextureBig = Content.Load<Texture2D>("fedExJetBig");
+
+            fedExJetTextureSmall = Content.Load<Texture2D>("fedExJetSmall");
+
+            explosionTexture = Content.Load<Texture2D>("explosion");
+
             // TODO: use this.Content to load your game content here
         }
 
@@ -65,6 +96,14 @@ namespace Delivery_Dog_Fight
         {
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            prevMouseState = mouseState;
+           
+            mouseState = Mouse.GetState();
+            
+            this.Window.Title = "" + mouseState.X + "," + mouseState.Y; 
+
+            
 
             // TODO: Add your update logic here
 
@@ -74,6 +113,18 @@ namespace Delivery_Dog_Fight
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(cockpitTexture, cockpitRect, Color.White);
+
+            _spriteBatch.Draw(upsJetTextureBig, upsJetBigRect, Color.White);
+
+            _spriteBatch.Draw(upsJetTextureSmall, upsJetSmallRect, Color.White);
+
+            _spriteBatch.Draw(upsJetTextureSmall, upsJetSmallRect, Color.White);
+
+            _spriteBatch.End();
 
             // TODO: Add your drawing code here
 
